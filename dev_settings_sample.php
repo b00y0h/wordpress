bec31ff01cce15bb105492409b0f98676a69633b
Bobby Smith <reversetype@gmail.com>
2012-06-27 11:38:47 -0400

    add dev_settings_sampl

---
+++ dev_settings_sample.php
@@ -0,0 +1,15 @@
+<?php
+
+$debug_on = true;
+
+/** The name of the database for WordPress */
+define('DB_NAME', '');
+
+/** MySQL database username */
+define('DB_USER', '');
+
+/** MySQL database password */
+define('DB_PASSWORD', '');
+
+/** MySQL hostname */
+define('DB_HOST', 'localhost');
