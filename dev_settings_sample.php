4ff3854bea90da9b5359d226f6029c4a9c8a9aa6
Bobby Smith <reversetype@gmail.com>
2013-11-12 15:17:56 -0500

    add db defaults

--- dev_settings_sample.php
+++ dev_settings_sample.php
@@ -1,6 +1,11 @@
 <?php
-
+define('FS_METHOD','direct');
+$d = $_SERVER['SERVER_NAME'];
+$db = $d;
 $debug_on = true;
+// define('DB_USER', 'root');
+// define('DB_PASSWORD', '');
+// define('DB_HOST', '127.0.0.1');
 
 // disable revisions
 define('WP_POST_REVISIONS', false );
